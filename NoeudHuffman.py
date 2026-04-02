4e3587aefb8a35d2dbdd2fdea3ed21f0061b43b7
n_lefebvre <nell.lefebvre@etu.u-pec.fr>
2026-04-02 16:58:41 +0200

    Création d'une to do list, correction de la fonction compteOccurrences(chaine).

--- NoeudHuffman.py
+++ NoeudHuffman.py
@@ -28,13 +28,42 @@ class NoeudHuffman(NoeudBinaire):
         self.valeur = (chaine_, poids_)
         
     #Méthodes
-    def compter_occurrences(texte):
-        freqs = {}
-        for c in texte:
-            freqs[c] = freqs.get(c, 0) + 1
-   
+    """
+    Construire un arbre de Huffman à partir d'une chaîne de caractères reçue en paramètre :
+        - Compter le nombre d'occurence de chaque caractère (stocker)
+        - Trier caractère en ordre décroissant d'occurence (stocker)
+        - Par pair (et deux pairs par deux pairs) concaténer les caractères
+            et additionner leurs poids
+        - Ajouter à l'arbre au moment de la concaténation les caractères seuls
+            et leurs nouveaux parents (niveau 1)
+        - Continuer ainsi jusqu'à avoir la chaine entière en racine
+        - permettre l'affichage de cet arbre
+        
+    A partir d'une chaine de caractères et d'un arbre d'Huffman associé,
+    calculer le nouvel encodage après compression de chaque caractère :
+        - Utiliser l'arbre (et les relation noeud - sous-arbre) pour retracer l'encodage (stocker)
+        - Potentiellement le rendre disponible à l'aide des différents parcours?
+        
+    A partir d'une chaine de caractères et d'un encodage d'Huffman associé,
+    construire et renvoyer la chaîne de caracère obtenu après compression :
+        - Fonction qui encode une chaine avec un encodage prédéfini?
+        
+    Bonus :
+        - afficher les 0 et les 1 dans l'affichage de l'arbre d'Huffman
+    """
+    
+    def compteOccurrences(chaine):
+        frequence = dict()
+        for caractere in chaine:
+            if caractere in frequence.keys():
+                frequence[caractere] += 1
+            else:
+                frequence[caractere] = 1
+        return frequence
+
    
 #Tests
+    #Initialisation d'un arbre (exemple tiré du cahier technique)
 v = NoeudHuffman("r", 2, None, None)
 w = NoeudHuffman("n", 2, None, None)
 x = NoeudHuffman("b", 2, None, None)
@@ -51,6 +80,7 @@ c = NoeudHuffman("o", 2, None, None)
 b = NoeudHuffman("oijsu", 6, c, d)
 a = NoeudHuffman("oijsunrb", 14, b, z)
 
+    #Constructeur, getters, setters
 print("La valeur de la racine est : ", a.getValeur())
 print("Sa chaîne est : ", a.getChaine())
 print("Son poids est : ", a.getPoids())
@@ -62,3 +92,4 @@ print("Test de setChaine() et de setPoids()")
 a.setChaine("oijsunrb")
 a.setPoids(14)
 print("Sa nouvelle valeur est donc : ", a.getValeur())
+print(a.__str__())
