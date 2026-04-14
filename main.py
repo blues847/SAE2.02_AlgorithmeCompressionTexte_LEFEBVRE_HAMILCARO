f438e2bb7653b39f4924a08fe0826777434778b4
n_lefebvre <nell.lefebvre@etu.u-pec.fr>
2026-04-15 01:57:45 +0200

    traiter_fichier() fait, ajout de commentaire dans tout le programme. Il reste le main() à faire pour l'instant et voir s'il ne serait pas intéressant d'afficher les stats dans un fichier en plus...? De stocker ces informations ailleurs.

--- main.py
+++ main.py
@@ -9,11 +9,19 @@ def compresser_texte(texte):
     Renvoie les éléments clés. Prend en paramètre le texte
     sous forme d'un chaîne de caractères.
     """
+    # Création du dictionnaire d'occurences des caractères
     dico = NoeudHuffman.compte_Occurences(texte)
+    
+    # Création de l'arbre de Huffman
     racine = NoeudHuffman.concatenation(dico)
+    
+    # Création du nouvel encodage
     codes = NoeudHuffman.generer_codes(racine)
+    
+    # Compression du texte
     texte_compresse = NoeufHuffman.compresser(texte, codes)
     return texte_compresse, racine
+
 
 def afficher_stat(texte_propre, texte_compresse):
     """
@@ -33,6 +41,7 @@ def afficher_stat(texte_propre, texte_compresse):
     else:
         print("Fichier vide, pas de compression.\n")
     
+    
 def verifier_decompression(texte_propre, texte_compresse, racine):
     """
     Décompresse le texte et vérifie qu'il correspond à l'original.
@@ -47,15 +56,42 @@ def verifier_decompression(texte_propre, texte_compresse, racine):
         print("TEST OK : texte avant / après compression cohérent")
     else:
         print("ERREUR : incohérence.s après décompression")
-    
+  
+  
 def traiter_fichier(chemin_fichier):
     """
     Appelle les focntions pour gérer le traitement complet d'un fichier.
     Prends en paramètres le chemin du fichier."
     """
-    # Nettoyage ASCII
-    texte = nettoyer_ascii(texte_original)
+    print(f"Fichier : {os.path.basename(chemin_fichier)}")
     
+    # Lecture du fichier, gestion d'une potentielle erreur
+    try:
+        with open(chemin_fichier, 'r', encoding='utf-8') as file:
+            texte_original = file.read()
+    except Exception as e:
+        print(f"Erreur de lecture : {e}")
+        return
+    
+    # Nettoyage ASCII
+    texte_propre = nettoyer_ascii(texte_original)
+    if not texte_propre:
+        print("Le fichier est vide ou non compatible.")
+        return
+    
+    # Compression
+    texte_compresse, racine = compresser_texte(texte_propre)
+    
+    # Affichage des résultats
+    afficher_stat(texte_propre, texte_compresse)
+    
+    # Vérification
+    verifier_decompression(texte_propre, texte_compresse, racine)
+    
+    #Barre de fin (séparation dans la console)
+    print("=" * 30 + "\n")
+ 
+ 
 def main():
 
 
