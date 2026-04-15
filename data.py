a484e106433b1933a54ebfb62f53765e44ea5d3f
HAMILCARO <hakim.hamilcaro@etu.u-pec.fr>
2026-04-15 09:42:41 +0200

    Data update

--- data.py
+++ data.py
@@ -1,34 +1,3 @@
-def nettoyer_ascii(texte, utiliser_unidecode=True):
-    """
-    Nettoie un texte pour le rendre ASCII.
-    - Essaie unidecode si demandé et disponible
-    - Sinon utilise une méthode de secours
-    """
-
-    if utiliser_unidecode:
-        try:
-            from unidecode import unidecode
-            return unidecode(texte)
-        except ModuleNotFoundError:
-            print("[INFO] unidecode non disponible, utilisation du programme de secours.")
-
-    # --- Méthode de secours (toujours exécutée si unidecode échoue ou est désactivé)
-    remplacements = {
-        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
-        'à': 'a', 'â': 'a',
-        'ù': 'u', 'û': 'u',
-        'î': 'i', 'ï': 'i',
-        'ô': 'o',
-        'ç': 'c',
-        'É': 'E', 'È': 'E', 'À': 'A', 'Ç': 'C'
-    }
-
-    for car, rep in remplacements.items():
-        texte = texte.replace(car, rep)
-
-    return texte
-
-
 class Data:
     """
     Classe utilitaire pour calculer les tailles
@@ -60,6 +29,37 @@ class Data:
         if taille_initiale == 0:
             return 0
         return taille_compressee / taille_initiale
+    
+    @staticmethod
+    def nettoyer_ascii(texte, utiliser_unidecode=True):
+    """
+    Nettoie un texte pour le rendre ASCII.
+    - Essaie unidecode si demandé et disponible
+    - Sinon utilise une méthode de secours
+    """
+
+    if utiliser_unidecode:
+        try:
+            from unidecode import unidecode
+            return unidecode(texte)
+        except ModuleNotFoundError:
+            print("[INFO] unidecode non disponible, utilisation du programme de secours.")
+
+    # --- Méthode de secours (toujours exécutée si unidecode échoue ou est désactivé)
+    remplacements = {
+        'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e',
+        'à': 'a', 'â': 'a',
+        'ù': 'u', 'û': 'u',
+        'î': 'i', 'ï': 'i',
+        'ô': 'o',
+        'ç': 'c',
+        'É': 'E', 'È': 'E', 'À': 'A', 'Ç': 'C'
+    }
+
+    for car, rep in remplacements.items():
+        texte = texte.replace(car, rep)
+
+    return texte
 
 
 # =====================================================
