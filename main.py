3c19dc255bb2030b7153a6da281c09d9554ab8bf
n_lefebvre <nell.lefebvre@etu.u-pec.fr>
2026-04-15 02:29:10 +0200

    main() fait. Prochaine étape : améliorer les affichages dans le terminal (afficher l'arbre d'Huffman / afficher l'encodage des caractères / les print en général).

--- main.py
+++ main.py
@@ -93,6 +93,27 @@ def traiter_fichier(chemin_fichier):
  
  
 def main():
+    """
+    Cette fonction se charge d'activer le programme de compression et est le lien
+    entre le terminal et les fonctions python.
+    """
+    # Vérification que l'utilisateur fourni un argument
+    if len(sys.argv) != 2:
+        print("Mauvaise saisie!")
+        print("Utilisation : python main.py <dossier_input>")
+        
+    # Récupération du chemin (argument)
+    dossier_input = sys.argv[1]
+    if not os.path.isdir(dossier_input):
+        print(f"ERREUR : le dossier n'existe pas.")
+        return
+    
+    # Parcours de la liste de tous les fichiers / dossiers
+    for nom_fichier in os.listdir(dossier_input):
+        if nom_fichier.endswith('.txt'): #Si c'est un fichier texte
+            chemin_complet = os.path.join(dossier_input, nom_fichier)
+            #Lancement du programme en soi
+            traiter_fichier(chemin_complet)
 
 
 if __name__ == "__main__":
